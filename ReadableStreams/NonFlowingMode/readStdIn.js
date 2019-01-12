0cdfe3d62f4d7140e64153d6f23a916335d86246
rkterungwa16 <terungwakombol@gmail.com>
2019-01-12 04:27:59 +0100

    illustrate stream flowing mode and non flowing mode

---
+++ ReadableStreams/NonFlowingMode/readStdIn.js
@@ -0,0 +1,27 @@
+
+/**
+ * Attach a listener for the 'readable' event that
+ * signal the availability of new data to read.
+ */
+process.stdin.on('readable', () => {
+  // Readall the data until the internal buffer is emptied
+
+  // read() method synchronously reads from the internal
+  // buffers and returns a Buffer or String object
+  // representing a chunk of data
+
+  // read() method is a synchronous operation that
+  // pulls a data chunk from the internal buffers of the
+  // Readable stream
+  const chunk = process.stdin.read()
+  console.log('New data available', chunk)
+  while (chunk) {
+    console.log(
+      `Chunk read: (${chunk.length}) "${
+        chunk.toString()}"`
+    )
+  }
+})
+process.stdin.on('end', () => {
+  process.stdout.write('End of stream')
+})
